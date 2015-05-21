0e4e1b3f3c1297a1215229869e4a1a97454dbeb5
Peter Butler <peterebutler@gmail.com>
2015-05-21 16:44:22 -0600

    Initial commit

---
+++ routes.js
@@ -0,0 +1,12 @@
+var JSX = require('node-jsx').install(),
+  React = require('react')
+
+module.exports = {
+
+  index: function(req, res) {
+      // Render our 'home' template
+      res.render('home', {
+       });
+
+  },
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+// Require our dependencies
+var express = require('express'),
+  exphbs = require('express-handlebars'),
+  http = require('http'),
+  mongoose = require('mongoose'),
+  routes = require('./routes');
+
+// Create an express instance and set a port variable
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Set handlebars as the templating engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Connect to our mongo database
+mongoose.connect('mongodb://localhost/groceries');
+
+// Index Route
+app.get('/', routes.index);
+
+// Set /public as our static content dir
+app.use("/", express.static(__dirname + "/public/"));
+
+// Start server
+var server = http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
+
+// Initialize socket.io
+var io = require('socket.io').listen(server);
